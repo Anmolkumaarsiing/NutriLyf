33e6ddda22edd2c5b9d892ee3f839e324d5e5524
Anmol Kumar Singh <124506883+Anmolkumaarsiing@users.noreply.github.com>
2024-07-11 19:58:46 +0530

    Update login.js

--- LOGIN/login.js
+++ LOGIN/login.js
@@ -17,7 +17,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
             });
     
             if (validUser) {
-                window.location.href = 'NutriLyf/DASHBOARD/DASHBOARD.HTML';
+                window.location.href = '../DASHBOARD/DASHBOARD.HTML';
             } else {
                 alert('Invalid email or password');
             }
